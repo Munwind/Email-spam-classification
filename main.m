d31ca83853ca4c0190230a6f572ffffe5355f9cd
Pham Hoang <duyhoangpham2610@gmail.com>
2024-05-13 16:25:53 +0700

    Preprocess -> Feature extraction

--- main.m
+++ main.m
@@ -7,6 +7,20 @@ data.Message = lower(data.Message);
 data.Message = regexprep(data.Message, '[^\w\s]', '');
 data.TokenizedMessage = cellfun(@(x) strsplit(x), data.Message, 'UniformOutput', false);
 
+% Delete stop words and stemming
+
+
+
+
+
+
+% Feature extraction there  use TF - IDF
+
+
+
+
+
+
 % Get the label
 data.Label = categorical(data.Category); % Convert labels to categorical
 data.Label = double(data.Label == 'spam'); % Encode 'spam' as 1 and 'ham' as 0
@@ -14,4 +28,10 @@ data.Label = double(data.Label == 'spam'); % Encode 'spam' as 1 and 'ham' as 0
 % Split the data into training and testing
 split_data;
 
-%disp(trainData(1:5, :));
+
+% Choose model there
+
+
+
+
+
